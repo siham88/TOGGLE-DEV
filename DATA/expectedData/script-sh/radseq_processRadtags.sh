e3bdd38ad077936c99ef0767b11572ea3535f9a8
julieaorjuela <julieaorjuela@gmail.com>
2015-06-15 11:01:52 +0200

    modifying command line to process radtags in data test DATA/expectedDATA/script-sh/radseq_processRadtags.sh

--- DATA/expectedData/script-sh/radseq_processRadtags.sh
+++ DATA/expectedData/script-sh/radseq_processRadtags.sh
@@ -1,2 +1,10 @@
+rm -rf ../radseq/outputRadseq/
 mkdir ../radseq/outputRadseq/
-/usr/local/stacks-1.29/bin/process_radtags -f ../radseq/C05F2ACXX_5_fastq -o ../radseq/outputRadseq/ -b ../radseq/barcode_line5.txt -e apeKI -i fastq
+
+/usr/local/stacks-1.29/bin/process_radtags -p ../radseq/radseqInput/lane5/ -o ../radseq/outputRadseq/ -b ../radseq/radseqInput/barcode/barcode_run01_lane5.txt -e apeKI -i fastq
+
+mv ../radseq/outputRadseq/process_radtags.log ../radseq/outputRadseq/process_radtags_lane5.log
+
+/usr/local/stacks-1.29/bin/process_radtags -p ../radseq/radseqInput/lane6/ -o ../radseq/outputRadseq/ -b ../radseq/radseqInput/barcode/barcode_run01_lane6.txt -e apeKI -i fastq
+
+mv ../radseq/outputRadseq/process_radtags.log ../radseq/outputRadseq/process_radtags_lane6.log
