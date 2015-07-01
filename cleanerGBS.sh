16dba9c8ebb56876cd3ec7eadac1269fe8448b06
julieaorjuela <julieaorjuela@gmail.com>
2015-07-01 14:32:39 +0200

    adding cleanerGBS.sh

---
+++ cleanerGBS.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mv DATA-TEST/radseq/data_radseq/lane5/C05F2ACXX* DATA-TEST/radseq/data_radseq/
+mv DATA-TEST/radseq/data_radseq/lane6/C08L7ACXX_6.fastq DATA-TEST/radseq/data_radseq/
+rm -rf DATA-TEST/radseq/data_radseq/lane*
+rm  -rf DATA-TEST/radseq/data_radseq/outputRadseq/
+
