735ef6d65c19e30b4152190a623bd6323f67a1a4
sravel <sebastien.ravel@cirad.fr>
2015-09-23 11:03:37 +0200

    add prove -v radseq_test.t to all_tests.sh and replace localConfig.pm by default

--- Modules/localConfig.pm
+++ Modules/localConfig.pm
@@ -31,7 +31,6 @@ package localConfig;
 ###################################################################################################################################
 
 
-
 use strict;
 use warnings;
 use Exporter;
@@ -40,38 +39,38 @@ our @ISA=qw(Exporter);
 our @EXPORT=qw($bwa $picard $samtools $GATK $cutadapt $fastqc $java $toggle $fastxTrimmer $tophat2 $bowtie2Build $bowtieBuild $htseqcount $radseq);
 
 #toggle path
-our $toggle="/home/tranchant/TOGGLE";
+our $toggle="/path/to/toggle";
 
 #PATH for Mapping on cluster
-our $java = "/usr/local/java/latest/bin/java -Xmx12g -jar";
+our $java = "/path/to/java -Xmx12g -jar";
 
-our $bwa = "/usr/local/bin/bwa";
-our $picard = "$java /usr/local/picard-tools-1.130/";
+our $bwa = "/path/to/bwa";
+our $picard = "$java /path/to/picard-tools-1.130";
 
-our $samtools = "/usr/local/samtools-1.2/bin/samtools";
-our $GATK = "/usr/java/jre1.7.0_51/bin/java -Xmx12g -jar /usr/local/GenomeAnalysisTK-3.3/GenomeAnalysisTK.jar";
-our $fastqc = "/usr/local/FastQC/fastqc";
+our $samtools = "/path/to/samtools_1.2";
+our $GATK = "/path/to/java -Xmx12g -jar /path/to/GenomeAnalysisTK-3.3/GenomeAnalysisTK.jar";
+our $fastqc = "/path/to/FastQC/fastqc";
 
 #Path for CutAdapt
-our $cutadapt = "/usr/local/cutadapt-1.8/bin/cutadapt";
+our $cutadapt = "/path/to/cutadapt-1.8/bin/cutadapt";
 
 ##### FOR RNASEQ analysis
 #Path for fastq_trimmer
-our $fastxTrimmer="/usr/local/bin/fastx_trimmer";
+our $fastxTrimmer="/path/to/bin/fastx_trimmer";
 
 #Path for tophat2
-our $tophat2="/usr/local/tophat-2.0.14/bin/tophat2";
-#
+our $tophat2="/path/to/tophat2";
+
 #path for bowtie2-build
-our $bowtie2Build="/usr/local/bowtie2-2.2.5/bowtie2-build";
+our $bowtie2Build="/path/to/bowtie2-build";
 
 #path for bowtie-build
-our $bowtieBuild="/usr/local/bowtie-0.12.9/bowtie-build";
+our $bowtieBuild="/path/to/bowtie-build";
 
 #path for htseqcount
-our $htseqcount = "/usr/local/bin/htseq-count";
+our $htseqcount = "/path/to/htseq-count";
 
 #path for radseq
-our $radseq = "/usr/local/stacks-1.29/bin/process_radtags";
+our $radseq = "/path/to/stacks-1.29/bin/process_radtags";
 
 1;
