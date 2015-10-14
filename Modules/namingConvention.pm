59c9cadf170d3c039d613402377825f87cb5acd6
tranchant <christine.tranchant@ird.fr>
2015-10-14 08:52:42 +0200

    add tophat2build

--- Modules/namingConvention.pm
+++ Modules/namingConvention.pm
@@ -139,6 +139,7 @@ sub correctName
 
         #FOR tophat.pm
         case ($name =~ m/bowtie[\s|\.|\-| \/|\\|\|]*build/i){$correctedName="bowtieBuild"; } #Correction for bwaAln
+	case ($name =~ m/bowtie[\s|\.|\-| \/|\\|\|]*build/i){$correctedName="bowtie2Build"; } #Correction for bwaAln
         #FOR cufflinks.pm
         
         #FOR HTSeq.pm
