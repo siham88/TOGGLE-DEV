471af1f0901593fb235c877381f4b40048f78990
tranchant <christine.tranchant@ird.fr>
2015-11-25 14:51:41 +0100

    copy into finalResults directory : add option -rf for copy and remove

--- toggleGenerator.pl
+++ toggleGenerator.pl
@@ -558,7 +558,7 @@ if ($orderBefore1000)
                 next if (not defined $file or $file =~ /^\s*$/);	
 		$file =~s/://g;
 		my ($basicName)=toolbox::extractPath($file);
-                my $cpLnCommand="cp -rf $file $finalDir/$basicName && rm -f $file && ln -s $finalDir/$basicName $file";
+                my $cpLnCommand="cp -rf $file $finalDir/$basicName && rm -rf $file && ln -s $finalDir/$basicName $file";
                 ##DEBUG toolbox::exportLog($cpLnCommand,1);
                 ##DEBUG
                 toolbox::exportLog("--------->".$file."-".$basicName,2);
@@ -687,7 +687,7 @@ if ($orderAfter1000)
     foreach my $file (@{$fileList}) #Copying the final data in the final directory
     {
         my ($basicName)=toolbox::extractPath($file);
-        my $cpLnCommand="cp $file $finalDir/$basicName && rm -rf $file && ln -s $finalDir/$basicName $file";
+        my $cpLnCommand="cp -rf $file $finalDir/$basicName && rm -rf $file && ln -s $finalDir/$basicName $file";
         ##DEBUG toolbox::exportLog($cpLnCommand,1);
         if(toolbox::run($cpLnCommand))       #Execute command
         {
