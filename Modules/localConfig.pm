dc60fbf442c9c90d149e87e2a6e63a9126a96f85
christine <christine.tranchant@ird.Fr>
2015-12-09 09:37:17 +0100

    Update generic localConfig.pm

--- Modules/localConfig.pm
+++ Modules/localConfig.pm
@@ -37,38 +37,43 @@ use warnings;
 use Exporter;
 
 our @ISA=qw(Exporter);
-our @EXPORT=qw($bwa $picard $samtools $GATK $cutadapt $fastqc $java $toggle $fastxTrimmer $tophat2 $bowtie2Build $bowtieBuild $htseqcount);
+our @EXPORT=qw($bwa $picard $samtools $GATK $cutadapt $fastqc $java $toggle $fastxTrimmer $tophat2 $bowtie2Build $bowtieBuild $htseqcount $cufflinks $cuffdiff $cuffmerge);
 
 #toggle path
-our $toggle="/home/tranchant/TOGGLE-ONTHEFLY";
+our $toggle="/path/to/toggleFolder";
 
 #PATH for Mapping on cluster
-our $java = "/usr/local/java/latest/bin/java -Xmx12g -jar";
+our $java = "/path/to/java -Xmx12g -jar";
 
-our $bwa = "/usr/local/bin/bwa";
-our $picard = "$java /usr/local/picard-tools-1.130";
+our $bwa = "/path/to/bwa";
+our $picard = "$java /path/to/picard_tools/";
 
-our $samtools = "/usr/local/samtools-1.2/bin/samtools";
-our $GATK = "/usr/java/jre1.7.0_51/bin/java -Xmx12g -jar /usr/local/GenomeAnalysisTK-3.3/GenomeAnalysisTK.jar";
-our $fastqc = "/usr/local/FastQC/fastqc";
+our $samtools = "/path/to/samtools";
+our $GATK = "$java -Xmx12g -jar /path/to/GenomeAnalysisTK.jar";
+our $fastqc = "/path/to/fastqc";
 
 #Path for CutAdapt
-our $cutadapt = "/usr/local/cutadapt-1.8/bin/cutadapt";
+our $cutadapt = "/path/to/cutadapt";
 
 ##### FOR RNASEQ analysis
 #Path for fastq_trimmer
-our $fastxTrimmer="/usr/local/bin/fastx_trimmer";
+our $fastxTrimmer="/path/to/fastx_trimmer";
 
 #Path for tophat2
-our $tophat2="/usr/local/tophat-2.0.14/bin/tophat2";
+our $tophat2="/path/to/tophat2";
 
 #path for bowtie2-build
-our $bowtie2Build="//usr/local/bowtie2-2.2.5/bowtie2-build";
+our $bowtie2Build="/path/to/bowtie2-build";
 
 #path for bowtie-build
-our $bowtieBuild="/usr/local/bowtie-0.12.9/bowtie-build";
+our $bowtieBuild="/path/to//bowtie-build";
 
 #path for htseqcount
-our $htseqcount = "/usr/local/bin/htseq-count";
+our $htseqcount = "/path/to//htseq-count";
+
+#path for Cufflinks
+our $cufflinks = "/path/to/cufflinks";
+our $cuffdiff = "/path/to/cuffdiff";
+our $cuffmerge = "/path/to/cuffmerge";
 
 1;
