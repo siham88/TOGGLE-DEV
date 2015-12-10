31d21c97f6a6d4c4a68944392a9359b5c2b6ea8d
Francois Sabot <francois.sabot@ird.fr>
2015-12-10 17:06:07 +0100

    Almost finished the SGE scheduling through scheduler...

--- Modules/scheduler.pm
+++ Modules/scheduler.pm
@@ -45,11 +45,20 @@ use Switch;
 #
 ##################################
 
-our ($commandLine, $requirement, $sample, $configInfo);
+our ($commandLine, $requirement, $sample, $configInfo, $jobList, %jobHash);
+
+#Requirement means the job needs to be achieved for the next steps or it is not a blocking job?
+# A zero value (0) means mandatory
+# Any non-zero value means not blocking job
 
 sub launcher { #Global function for launching, will recover the command to be launch and will re-dispatch to normal or other scheduler
     
     ($commandLine,$requirement, $sample, $configInfo) = @_;
+    
+    #Picking up sample name
+    
+    $sample=`basename $sample` or die("ERROR : $0 : Cannot pickup the basename for $sample: $!\n");
+    chomp $sample;
     
     my $hashCapability = &checkingCapability;
     my $runOutput;
@@ -60,6 +69,10 @@ sub launcher { #Global function for launching, will recover the command to be la
         
         #If no scheduler available or configurated in the config info file, let's run it in a normal way
         else {$runOutput = &normalRun};
+    }
+    
+    if ($runOutput == 0 && $requirement == 1) {#The job has to succeed either it will kill all other jobs
+        toolbox::exportLog("WARNING: scheduler::launcher on $sample: ".$commandLine."\nThe job cannot be achieved and is mandatory, thus the whole analysis is stop\n",0);
     }
     
     return $runOutput;
@@ -94,17 +107,17 @@ sub normalRun { #For running in normal mode, ie no scheduler
     my ($result,$stderr)=capture {` $commandLine `};
     
     ##Log export according to the error
-    if ($?==0)
+    if ($?==0) #Success, no error
     {
 	##DEBUG
-	exportLog("INFOS: scheduler::normalRun on $sample: ".$result."\n--".$stderr."\n",1);
+	toolbox::exportLog("INFOS: scheduler::normalRun on $sample: ".$result."\n--".$stderr."\n",1);
 	return 1;
     }
-    else
+    else  #Error, the job cannot be achieved for any reason
     {
 	##DEBUG
-	exportLog("WARNING: scheduler::normalRun on $sample: ".$result."\n--".$stderr."\nThe $sample data set has provoked and error, and was not analyzed anymore.\n",2);
-	return 2;
+	toolbox::exportLog("WARNING: scheduler::normalRun on $sample: ".$result."\n--".$stderr."\nThe $sample data set has provoked and error, and was not analyzed anymore.\n",2);
+	return 0;
     }
     
 }
@@ -113,6 +126,48 @@ sub sgeRun { #For SGE cluster, running using qsub
     
     my $sgeOptionsHash=toolbox::extractHashSoft($configInfo,"sge");
     my $sgeOptions=toolbox::extractOptions($sgeOptionsHash);
+    
+    #Adding qsub options
+    my $launcherCommand = "qsub ".$sgeOptions." ".$commandLine;
+    $launcherCommand =~ s/ +/ /g; #Replace multiple spaces by a single one, to have a better view...
+    my $currentJID = `$launcherCommand`;
+    
+    if ($!) #There are errors in the launching...
+    {
+        warn ("WARNING : $0 : Cannot launch the job for $sample: $!\n");
+        $currentJID = "";
+    }
+        
+    #Parsing infos and informing logs
+    chomp $currentJID;
+    
+    unless ($currentJID) #The job has no output in STDOUT, ie there is a problem...
+    {
+        return 0; #Returning to launcher subprogram the error type
+    }
+    
+    toolbox::exportLog("INFOS: $0 : Correctly launched for $sample in qsub mode through the command:\n\t$launcherCommand\n\n",1);
+    toolbox::exportLog("INFOS: $0 : Output for the command is $currentJID\n\n",1);
+    
+    my @infosList=split /\s/, $currentJID; #the format is such as "Your jobID ("NAME") has been submitted"
+    $currentJID = $infosList[2];
+    
+    my $runningNodeCommand="qstat | grep $currentJID";
+    my $runningNode="";
+    while ($runningNode eq "") #If the job is not yet launched, there is no node affected, $runningMode is empty
+    {
+      sleep 5;#Waiting for the job to be launched
+      $runningNode=`$runningNodeCommand` or warn("WARNING : $0 : Cannot pickup the running node for $currentJID: $!\n");
+      chomp $runningNode;
+      my @runningFields = split / +/,$runningNode; #To obtain the correct field
+      next if $runningFields[4] ne "r"; # not running
+      $runningNode = $runningFields[7];
+      $runningNode =~ s/.+@//;#removing queue name providing only node name
+    }
+    toolbox::exportLog("INFOS: $0 : Running node for job $currentJID is $runningNode\n\n",1);
+    
+    #Provide the job ID
+    return $currentJID;
 }
 
 sub slurmRun{ #for SLURM cluster, running using sbatch
@@ -128,8 +183,77 @@ sub slurmRun{ #for SLURM cluster, running using sbatch
 #
 ##################################
 
+sub waiter { #Global function for waiting, will recover the jobID to survey and will re-dispatch to scheduler
+    
+    ($jobList,my $jobsInfos) = @_;
+    
+    %jobHash = %{$jobsInfos};
+        
+    my $hashCapability = &checkingCapability;
+    my $stopWaiting;
+    switch (1)
+    {
+        case ($hashCapability->{"sge"} && $configInfo->{"sge"}){$stopWaiting = &sgeRun} #For SGE running
+        case ($hashCapability->{"slurm"} && $configInfo->{"slurm"}){$stopWaiting = &slurmRun} #For SLURM running
+    }
+   
+    return $stopWaiting;
+}
+
+
+
 sub sgeWait {
     
+    my $nbRunningJobs = 1;
+    ##Waiting for jobs to finish
+    while ($nbRunningJobs)
+    {  
+      ##DEBUG my $date = `date`;
+      ##DEBUG chomp $date;
+      ##DEBUG toolbox::exportLog("INFOS : $0 : $nbRunningJobs are still running at $date, we wait for their ending.\n",1);
+      #Picking up the number of currently running jobs
+      my $qstatCommand = "qstat | egrep -c \"$jobList\"";
+      $nbRunningJobs = `$qstatCommand`;
+      chomp $nbRunningJobs;
+      sleep 50;
+    }
+    
+    #Compiling infos about sge jobs: jobID, node number, exit status
+    sleep 25;#Needed for qacct to register infos...
+    toolbox::exportLog("INFOS: $0 : RUN JOBS INFOS\nIndividual\tJobID\tNode\tExitStatus\n-------------------------------",1);
+    foreach my $individual (sort {$a cmp $b} keys %jobHash)
+    {
+      my $qacctCommand = "qacct -j ".$jobHash{$individual};
+      my $qacctOutput = `$qacctCommand`;
+      chomp $qacctOutput;
+      my @linesQacct = split /\n/, $qacctOutput;
+      my $outputLine = $individual."\t".$jobHash{$individual}."\t";
+      while (@linesQacct) #Parsing the qacct output
+      {
+        my $currentLine = shift @linesQacct;
+        if ($currentLine =~ m/^hostname/) #Picking up hostname
+        {
+          $currentLine =~ s/hostname     //;
+          $outputLine .= $currentLine."\t";
+        }
+        elsif ($currentLine =~ m/^exit_status/) #Picking up exit status
+        {
+          $currentLine =~ s/exit_status  //;
+          $currentLine = "Normal" if $currentLine == 0;
+          $outputLine .= $currentLine;
+        }
+        else
+        {
+          next;
+        }
+        
+      }
+      toolbox::exportLog($outputLine,1);
+      
+    }
+    toolbox::exportLog("-------------------------------\n",1);#To have a better table presentation
+  
+  return 1;
 }
 
 sub slurmWait{
@@ -137,3 +261,44 @@ sub slurmWait{
 }
 
 1;
+
+=head1 NAME
+
+    Package I<scheduler> 
+
+=head1 SYNOPSIS
+
+	use scheduler;
+    
+	scheduler::launcher($launcherCommand, $requirement, $sample, $configInfo);
+    
+	scheduler::waiter($jobList,$jobInfos);
+
+=head1 DESCRIPTION
+
+    Package scheduler will prepare and launch the different command in a scheduler if possible and requested. Either, it will launch it through the normal way, using a quite identical way to toolbox::run
+    !!DIFFERENCE with toolbox::run is that an abnormal job will be a blocking one ONLY if requirement is of a zero value (0). Else, the job error will be warned but it will not block the whole system.
+
+=head2 FUNCTIONS
+
+=head3 scheduler::launcher
+
+This module will prepare and decide to which scheduler sending the command. It takes as arguments the command $launcherCommand, the state of the job (0 mandatory finishing, non-0 not mandatory) $requirement, the sample name $sample and the $configInfo hash for configuration
+
+=head3 scheduler::waiter
+
+This module will allow the job launched through a scheduler to be wait to finish.
+It takes as arguments the list of jobs $jobList and the reference of the hash for the informations about the jobs (sample name) $jobInfos
+
+
+
+=head1 AUTHORS
+
+Intellectual property belongs to IRD, CIRAD and South Green developpement plateform for all versions also for ADNid for v2 and v3 and INRA for v3
+Written by Cecile Monat, Christine Tranchant, Cedric Farcy, Maryline Summo, Julie Orjuela-Bouniol, Sebastien Ravel, Gautier Sarah, and Francois Sabot
+
+=head1 SEE ALSO
+
+L<http://www.southgreen.fr/>
+
+=cut
